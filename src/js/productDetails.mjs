614adb15f154ee74e3074ad80de8d5a4c6aab451
lyasquith <lydang26993@gmail.com>
2023-10-16 16:39:11 -0600

    update team2 assignment

--- src/js/productDetails.mjs
+++ src/js/productDetails.mjs
@@ -10,19 +10,19 @@ export default async function productDetails(productId){
 
     document.querySelector('#addToCart').addEventListener('click', addProductToCart);
 
-
 }
 
-function addProductToCart(product) {
+function addProductToCart() {
     setLocalStorage('so-cart', product);
   }
 
 function renderProductDetails(){
-    document.querySelector('#productName').innerText = product.Brand.Name;
+    document.querySelector('#productName').innerText = product.Brand.Name;//why Brand does not need index number?
     document.querySelector('#productNameWithoutBrand').innerText = product.NameWithoutBrand;
-    document.querySelector('#productImage').innerText = product.Image;//img is not working
+    document.querySelector('#productImage').src = product.Image;//img is not working if use innerText
     document.querySelector('#productFinalPrice').innerText = `$${product.FinalPrice}`;
-    document.querySelector('#productColorName').innerText = product.Colors.ColorName;// colorName is not working
-    document.querySelector('#productDescriptionHtmlSimple').innerText = product.DescriptionHtmlSimple;
-
+    document.querySelector('#productColorName').innerText = product.Colors[0].ColorName;// colorName is not working without index number
+    document.querySelector('#productDescriptionHtmlSimple').innerHTML = product.DescriptionHtmlSimple; //why use innerHTML instead of innerText. (weird icons appreared with innerText)
+    
 }
+// Can not add product to cart
