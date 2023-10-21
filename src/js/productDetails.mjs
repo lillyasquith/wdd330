84f0bfc69463a72f99225f1bdb3cc4024a9c4bb1
lyasquith <lydang26993@gmail.com>
2023-10-21 09:52:01 -0600

    update team2 assignment

--- src/js/productDetails.mjs
+++ src/js/productDetails.mjs
@@ -5,8 +5,8 @@ let product= {};
 
 export default async function productDetails(productId){
     product = await findProductById(productId);
-    console.log(product);
-    renderProductDetails();
+    //console.log(product);
+    renderProductDetails();//render the details of products
 
     document.querySelector('#addToCart').addEventListener('click', addProductToCart);
 
@@ -18,11 +18,11 @@ function addProductToCart() {
 
 function renderProductDetails(){
     document.querySelector('#productName').innerText = product.Brand.Name;//why Brand does not need index number?
-    document.querySelector('#productNameWithoutBrand').innerText = product.NameWithoutBrand;
-    document.querySelector('#productImage').src = product.Image;//img is not working if use innerText
-    document.querySelector('#productFinalPrice').innerText = `$${product.FinalPrice}`;
-    document.querySelector('#productColorName').innerText = product.Colors[0].ColorName;// colorName is not working without index number
-    document.querySelector('#productDescriptionHtmlSimple').innerHTML = product.DescriptionHtmlSimple; //why use innerHTML instead of innerText. (weird icons appreared with innerText)
+    //document.querySelector('#productNameWithoutBrand').innerText = product.NameWithoutBrand;
+    //document.querySelector('#productImage').src = product.Image;//img is not working if use innerText
+    //document.querySelector('#productFinalPrice').innerText = `$${product.FinalPrice}`;
+    //document.querySelector('#productColorName').innerText = product.Colors[0].ColorName;// colorName is not working without index number
+    //document.querySelector('#productDescriptionHtmlSimple').innerHTML = product.DescriptionHtmlSimple; //why use innerHTML instead of innerText. (weird icons appreared with innerText)
     
 }
 // Can not add product to cart issue //items showed as NULL in cart
