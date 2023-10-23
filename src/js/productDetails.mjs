738bbfae1a8232fe43437f4240b7fd6df223c716
lyasquith <lydang26993@gmail.com>
2023-10-23 17:04:15 -0600

    update team03 assignment

--- src/js/productDetails.mjs
+++ src/js/productDetails.mjs
@@ -17,11 +17,11 @@ function addProductToCart() {
   }
 
 function renderProductDetails(){
-    document.querySelector('#productName').innerText = product.Brand.Name;//why Brand does not need index number?
+    document.querySelector('#productName').innerText = product.Brand.Name;//why Brand does not need index number? B/c it is an Object
     document.querySelector('#productNameWithoutBrand').innerText = product.NameWithoutBrand;
     document.querySelector('#productImage').src = product.Image;//img is not working if use innerText
     document.querySelector('#productFinalPrice').innerText = `$${product.FinalPrice}`;
-    document.querySelector('#productColorName').innerText = product.Colors[0].ColorName;// colorName is not working without index number
+    document.querySelector('#productColorName').innerText = product.Colors[0].ColorName;// colorName is not working without index number. B/c it is an array
     document.querySelector('#productDescriptionHtmlSimple').innerHTML = product.DescriptionHtmlSimple; //why use innerHTML instead of innerText. (weird icons appreared with innerText)
     
 }
