2850f1a9b4fb0bd0e61b4ed1a4ff1f6339ae1b0a
lyasquith <lydang26993@gmail.com>
2023-11-14 16:24:13 -0700

    finished team6

--- src/js/checkoutProcess.mjs
+++ src/js/checkoutProcess.mjs
@@ -56,12 +56,13 @@ const checkoutProcess = {
     console.log(numItems);
   
     document.querySelector(".numItems").innerHTML = numItems;
-    document.querySelector(".cartTotal").innerHTML = `$${this.itemTotal}`;
+    document.querySelector(".cartTotal").innerHTML = `$${this.itemTotal.toFixed(2)}`;
 
   },
   calculateOrdertotal: function() {
     // calculate the tax amounts. Then use them to along with the cart total to figure out the order total
-    this.tax = this.itemTotal * 0.06
+    this.tax = (this.itemTotal * 0.06);
+
 
     // calculate the shipping
     const items = this.list.length;
@@ -70,15 +71,16 @@ const checkoutProcess = {
     } else {
       this.shipping = ((items - 1) * 2) + 10; // 4 items, 1 =>10, 3 * 2 , 16
     };
-    this.orderTotal = this.itemTotal + this.tax + this.shipping;
+    this.orderTotal = (this.itemTotal + this.tax + this.shipping);
+    
     // display the totals.
     this.displayOrderTotals();
   },
   displayOrderTotals: function() {
     // once the totals are all calculated display them in the order summary page
-    document.querySelector(".tax").innerHTML = this.tax;
-    document.querySelector(".shipping").innerHTML = this.shipping;
-    document.querySelector(".orderTotal").innerHTML = this.orderTotal;
+    document.querySelector(".tax").innerHTML = `$${this.tax.toFixed(2)}`;
+    document.querySelector(".shipping").innerHTML = `$${this.shipping.toFixed(2)}`;
+    document.querySelector(".orderTotal").innerHTML = `$${this.orderTotal.toFixed(2)}`;
   },
 
   checkout: async function(form){
@@ -103,7 +105,6 @@ const checkoutProcess = {
     //   console.log(err);
     // }
     
-
   }
   
   
